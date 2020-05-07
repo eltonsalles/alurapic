754af88f9be31a95fbcbb1a39d82526e5d28b324
Elton Rodrigues <eltonrms.leite@hotmail.com>
2020-05-07 06:47:31 -0300

    Adicionado bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Alurapic';
 
   url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR23EgR0Yd2nSkotp4Lb13svjgmXoGmGTQBqmWS7TpgTiCdo_GK&usqp=CAU';
 
