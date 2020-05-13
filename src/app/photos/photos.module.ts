1aa08ab82841ef715898a28f6cd26f10789f8b5d
Elton Rodrigues <eltonrms.leite@hotmail.com>
2020-05-13 06:41:38 -0300

    Criado submódulos.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PhotoComponent } from './photo/photo.component';
-import { HttpClientModule } from '@angular/common/http';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule
   ],
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescriptionPipe,
-    LoadButtonComponent
-  ],
-  exports: [
-    PhotoComponent
-  ]
+  declarations: [],
+  exports: []
 })
 export class PhotosModule { }
