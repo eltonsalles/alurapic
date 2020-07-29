d261ec2a65e66ebf708618dab34c02a6c25c72ae
Elton Rodrigues <eltonrms.leite@hotmail.com>
2020-07-28 23:31:57 -0300

    Criado o componente de alerta.

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Observable } from 'rxjs';
 import { PhotoModel } from '../photo/photo.model';
+import { AlertService } from '../../shared/components/alert/alert.service';
 
 @Component({
   templateUrl: './photo-details.component.html'
@@ -12,7 +13,12 @@ export class PhotoDetailsComponent implements OnInit {
   photo$: Observable<PhotoModel>;
   photoId: number;
 
-  constructor(private route: ActivatedRoute, private photoService: PhotoService, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private photoService: PhotoService,
+    private router: Router,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
     this.photoId = this.route.snapshot.params.photoId;
@@ -21,6 +27,9 @@ export class PhotoDetailsComponent implements OnInit {
 
   removePhoto() {
     this.photoService.removePhoto(this.photoId)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Photo removida');
+        this.router.navigate(['']);
+      });
   }
 }
